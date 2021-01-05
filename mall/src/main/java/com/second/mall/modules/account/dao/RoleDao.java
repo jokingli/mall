7624a8b5992b3bbc4bb56352605b5e8a72d7494c
2468893648 <2468893648@qq.com>
2021-01-05 10:48:55 +0800

    "roleDao"

--- mall/src/main/java/com/second/mall/modules/account/dao/RoleDao.java
+++ mall/src/main/java/com/second/mall/modules/account/dao/RoleDao.java
@@ -1,4 +1,8 @@
 package com.second.mall.modules.account.dao;
 
+import com.second.mall.modules.account.entity.Role;
+import com.second.mall.modules.account.entity.User;
+
 public interface RoleDao {
+    void insertRole(Role role);
 }
